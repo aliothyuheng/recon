d1609a61ddfd3b1851bbb130114d223b41b52b00
Yuheng Chen <yuhengch@umich.edu>
2015-10-13 10:24:33 -0400

    location

---
+++ location/lacation.py
@@ -0,0 +1,25 @@
+import csv
+
+#import file
+datafile = open('reaction_list.csv', 'r')
+datareader = csv.reader(datafile)
+data = []
+for row in datareader:
+    data.append(row)
+
+temp = []
+for x in range(1, len(data)):
+    string = data[x][12]
+    if string != "":
+         temp.append(string)
+
+result = list(set(temp))
+
+outfile = open('location.csv', 'wb')
+writer = csv.writer(outfile)
+writer.writerow(result)
+outfile.close()        
+
+
+
+
