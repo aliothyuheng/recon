6fc1addd76915f4b6c7238161cea3b180aa6c1f7
Yuheng Chen <yuhengch@umich.edu>
2015-12-08 23:42:12 -0500

    reaction with duplicate pathway

---
+++ reaction_duplicate_pathway/pathway_duplicate_remove.py
@@ -0,0 +1,45 @@
+import csv
+#find the reaction with duplicate pathway
+duplicate_pathway = []
+with open("reaction2Pathway_remove_duplication.csv", "rb") as f:
+    reader = csv.reader(f)
+    for line in reader:
+        if "," in line[1]:
+            duplicate_pathway.append(line[0])
+f.close()
+
+#read reaction ref
+ref = {}
+with open("reaction_ref.csv", "rb") as f:
+    reader = csv.reader(f,  quotechar='"')
+    for line in reader:
+        temp = line[1].split(",")
+        ref[line[0]] = temp
+f.close()
+
+#read original file
+original = []
+with open("reaction original.csv", "rb") as f:
+    reader = csv.reader(f, quotechar='"')
+    for line in reader:
+        original.append(line)
+f.close()
+
+#pull all the reaction with multiple pathways out of the original reaction table
+result = []
+for item in duplicate_pathway:
+    reaction = ref[item]
+    for i in reaction:
+        temp = []
+        temp.append(item)
+        temp.append(i)
+        temp.extend(original[int(i)][:4])
+        result.append(temp)
+        
+with open("reaction_pathway_duplicate.csv", "wb") as f:
+    writer = csv.writer(f)
+    writer.writerows(result)
+f.close()
+
+
+
